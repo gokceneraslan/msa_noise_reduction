8ffeb4bb57543b0f7da3a57f8223efd962b9c955
Gökçen Eraslan <gokcen.eraslan@gmail.com>
2012-12-21 21:52:12 +0100

    Add a simple bash script to generate tree and noise-reduced alignment files.

---
+++ generate_outputs.sh
@@ -0,0 +1,14 @@
+
+for d in inputs/*
+do
+    mkdir -p ${d/inputs/outputs}
+
+    for f in ${d}/*.msl
+    do
+        filename=`basename $f`
+        outfile=${f/inputs/outputs}
+
+        echo "Processing $f..."
+        ./reduce_noise -i "$f" -o $outfile -d ${outfile/msl/tree} -g ${f/msl/tree}
+    done
+done
