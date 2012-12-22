3b2ced942fa0096c24f4970ef939e80104da596c
Gökçen Eraslan <gokcen.eraslan@gmail.com>
2012-12-22 14:18:53 +0100

    Use phylotree class to render.

--- generate_outputs.sh
+++ generate_outputs.sh
@@ -10,6 +10,6 @@ do
         outfile=${f/inputs/outputs}
 
         echo "Processing $f..."
-        ./reduce_noise -i "$f" -o $outfile -d ${outfile/msl/tree} -g ${f/msl/tree}
+        ./reduce_noise -i "$f" -o $outfile -d ${outfile/msl/tree} -g ${f/msl/tree} -R svg
     done
 done
