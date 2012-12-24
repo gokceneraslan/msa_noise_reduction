4772d9c9248b8b86c3057f2fcbf5e2d655a13641
Başak Eraslan <basakelyildirim@gmail.com>
2012-12-24 20:19:30 +0100

    control files

---
+++ Control/generate_outputs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for d in inputs/*
+do
+    mkdir -p ${d/inputs/outputs}
+
+    for f in ${d}/*.msl
+    do
+        filename=`basename $f`
+        outfile=${f/inputs/outputs}
+
+        echo "Processing $f..."
+        ./reduce_noise -i "$f" -o $outfile -d ${outfile/msl/tree} -g ${f/msl/tree} -R svg -b muscle
+    done
+done
